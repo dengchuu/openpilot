d1a72f8447d0855fc80a13b4e4155585f35e5e7e
kegman <8837066+kegman@users.noreply.github.com>
2019-03-08 16:03:12 -0500

    add KpV and KiV

--- tune.py
+++ tune.py
@@ -38,7 +38,7 @@ button_delay = 0.2
 kegman = kegman_conf()
 kegman.conf['tuneGernby'] = "1"
 kegman.write_config(kegman.conf)
-param = ["react", "damp", "resist"]
+param = ["react", "damp", "resist", "KpV", "KiV"]
 
 j = 0
 while True:
