a9070ebcdc690ab9f7935c61b44dd229a52f12c3
kegman <8837066+kegman@users.noreply.github.com>
2019-03-11 13:00:45 -0400

    Update tune.py

--- tune.py
+++ tune.py
@@ -38,7 +38,7 @@ button_delay = 0.2
 kegman = kegman_conf()
 #kegman.conf['tuneGernby'] = "1"
 #kegman.write_config(kegman.conf)
-param = ["tuneGernby", "react", "damp", "resist", "Kp", "Ki"]
+param = ["tuneGernby", "react", "damp", "Kp", "Ki"]
 
 j = 0
 while True:
