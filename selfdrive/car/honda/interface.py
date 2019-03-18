345a80b9e52aedb4dc6941e739970d85a2f5d6e0
kegman <8837066+kegman@users.noreply.github.com>
2019-03-18 00:44:36 -0400

    Pilot tuning v0.5

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -314,8 +314,8 @@ class CarInterface(object):
       ret.steerRatio = 16.0         # as spec
       tire_stiffness_factor = 0.82
       ret.steerReactance = 0.055
-      ret.steerInductance = 0.01
-      ret.steerKpV, ret.steerKiV = [[0.45], [0.135]]
+      ret.steerInductance = 0.025
+      ret.steerKpV, ret.steerKiV = [[0.50], [0.22]]
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
