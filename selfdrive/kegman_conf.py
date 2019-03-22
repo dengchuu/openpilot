a93e8f292f3189aa4bdcd6d2f5d702ad5e7e52ce
kegman <8837066+kegman@users.noreply.github.com>
2019-03-21 23:29:07 -0400

    kegman_conf.py - add dampMPC and dampSteer

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -21,7 +21,8 @@ class kegman_conf():
       if "tuneGernby" not in self.config:
         self.config.update({"tuneGernby":"0"})
         self.config.update({"react":"-1"})
-        self.config.update({"damp":"-1"})
+        self.config.update({"dampMPC":"-1"})
+        self.config.update({"dampSteer":"-1"})
         self.config.update({"Kp":"-1"})
         self.config.update({"Ki":"-1"})    
         self.element_updated = True 
@@ -38,7 +39,7 @@ class kegman_conf():
     else:
       self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
                      "wheelTouchSeconds":"180", "battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
-                     "brakeStoppingTarget":"0.25", "tuneGernby":"0", "react":"-1", "damp":"-1", \
+                     "brakeStoppingTarget":"0.25", "tuneGernby":"0", "react":"-1", "dampMPC":"-1", "dampSteer":"-1", \
                      "Kp":"-1", "Ki":"-1"}
       self.write_config(self.config)
     return self.config
