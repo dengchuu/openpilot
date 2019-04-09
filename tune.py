4b3bbe82e77b0cef5d719d31989e0d49ba4741f0
kegman <8837066+kegman@users.noreply.github.com>
2019-04-09 19:20:21 -0400

    Add cameraOffset to tune.py & model_parse.py

--- tune.py
+++ tune.py
@@ -38,7 +38,7 @@ button_delay = 0.2
 kegman = kegman_conf()
 #kegman.conf['tuneGernby'] = "1"
 #kegman.write_config(kegman.conf)
-param = ["tuneGernby", "reactMPC", "dampMPC", "rateFF", "angleFF", "Kp", "Ki"]
+param = ["tuneGernby", "reactMPC", "dampMPC", "cameraOffset", "Kp", "Ki"]
 
 j = 0
 while True:
