182100cfcf98cc94ea502889a6b2cde8cde60bd3
Greg Esmond <gernby2002@yahoo.com>
2019-06-26 17:11:53 -0500

    more interface updates

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -73,6 +73,8 @@ class CarInterface(object):
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.steerRateCost = 0.5
+      ret.lateralTuning.pid.dampTime = 0.1
+      ret.lateralTuning.pid.reactMPC = -0.05
 
     if candidate == CAR.PRIUS:
       stop_and_go = True
@@ -99,7 +101,9 @@ class CarInterface(object):
       tire_stiffness_factor = 0.5533
       ret.mass = 3650 * CV.LB_TO_KG + std_cargo  # mean between normal and hybrid
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.05]]
-      ret.lateralTuning.pid.kf = 0.00007   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.dampTime = 0.1
+      ret.lateralTuning.pid.reactMPC = -0.05
 
     elif candidate == CAR.COROLLA:
       stop_and_go = False
