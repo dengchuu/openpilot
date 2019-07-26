1e10dc6375702d732e422d02f14aaa1886282125
Greg Esmond <gernby2002@yahoo.com>
2019-07-25 22:17:31 -0500

    updated interfaces

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -95,13 +95,13 @@ class CarInterface(object):
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.02]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
-    ret.lateralTuning.pid.dampTime = 0.1
-    ret.lateralTuning.pid.reactMPC = 0.1
+    ret.lateralTuning.pid.dampTime = 0.02
+    ret.lateralTuning.pid.reactMPC = 0.0
     ret.lateralTuning.pid.dampMPC = 0.25
     ret.lateralTuning.pid.rateFFGain = 0.2
-    ret.lateralTuning.pid.polyFactor = 0.01
+    ret.lateralTuning.pid.polyFactor = 0.001
     ret.lateralTuning.pid.polyDampTime = 0.2
-    ret.lateralTuning.pid.polyReactTime = 0.5
+    ret.lateralTuning.pid.polyReactTime = 1.0
 
     ret.enableCruise = False
 
